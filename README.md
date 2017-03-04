8f2705cc62f3dcd7dc986890cf06079381e03af4
Gil Mizrahi <soupiral@gmail.com>
2017-03-04 12:17:28 +0200

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+A standalone todomvc example cut out of [purescript-thermite](https://github.com/paf31/purescript-thermite) repo by Phil Freeman.
+
+### Prerequisites
+
+- psc-0.10.*
+- pulp-10.0.*
+- bower
+- npm
+
+### Building and Running
+
+```sh
+bower i
+npm i
+pulp browserify --to html/index.js
+cd html
+python -m SimpleHTTPServer # or any other http server
+```
+
+now go to: [localhost:8000](http://localhost:8000)
---
+++ src/Main.purs
@@ -0,0 +1,24 @@
+-- | This task list application is made up of two Thermite components:
+-- |
+-- | - a component for a single task (Components.Task)
+-- | - a component for a list of tasks (Components.TaskList)
+-- |
+-- | This example demonstrates the main features of a Thermite app:
+-- |
+-- | - Using `simpleSpec` to create simple components from `Render` and
+-- |   `PerformAction` functions
+-- | - Composing components using the `Monoid` instance and lens combinators.
+-- |
+-- | For each component we start by declaring action and state types.
+
+module Main (main) where
+
+import Prelude
+import Components.TaskList (initialTaskListState, taskList)
+import Control.Monad.Eff (Eff)
+import DOM (DOM) as DOM
+import Thermite as T
+
+-- | The main method creates the task list component, and renders it to the document body.
+main :: Eff (dom :: DOM.DOM) Unit
+main = T.defaultMain taskList initialTaskListState unit
